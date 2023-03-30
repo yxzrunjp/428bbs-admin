8b01e1875297c4bbd02f89738a8a09a973c1c29f
yxzrunjp <yxz894894@gmail.com>
2023-03-30 22:45:02 +0800

    配置打包

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
+const pageArr = ['NotFound', 'Login']
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -42,6 +42,29 @@ export default defineConfig({
           "^/api": "/api"
         }
       }
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('/views')) {
+            console.log(id);
+            const path = id.split('/views/')[1]
+            const name = path.split('/')[0]
+            if (pageArr.includes(name)) {
+              return name
+            } else {
+              return path.split('/')[1]
+            }
+          } else if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+        entryFileNames: 'static/[name].[hash].js',
+        chunkFileNames: 'static/js/[name].[hash].js',
+        assetFileNames: 'static/[ext]/[name].[hash].[ext]',
+      },
     }
   }
 })
